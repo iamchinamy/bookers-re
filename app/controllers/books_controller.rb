9d8cc592de0f189ddb419b30a8eb4b8db2d46db0
Chinami Kitagawa <cnm25.k@gmail.com>
2020-11-04 08:41:03 +0000

    create index & show function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,30 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      redirect_to books_path
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
