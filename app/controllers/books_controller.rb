131760ac7af5d8767ee687b67305441bcd6f1c75
Chinami Kitagawa <cnm25.k@gmail.com>
2020-11-05 05:28:57 +0000

    add edit function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,13 +13,27 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
-      redirect_to books_path
+      render action: :new
     end
   end
 
   def show
     @book = Book.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated'
+    else
+      render action: :edit
+    end
+  end
+
 
 private
   def book_params
